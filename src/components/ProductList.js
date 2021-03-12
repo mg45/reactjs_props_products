4042aa176474bc0a010a5c06fde573b0e40a8e7a
mg45 <m.golabski@gmx.de>
2021-03-12 15:25:57 +0100

    currency

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -8,19 +8,19 @@ const ProductList = () => {
                     <ProductItem
                         imgUrl={'https://source.unsplash.com/weekly?adidas'}
                         productTitle={'Product01'}
-                        productPrice={299}
+                        productPrice={299 + '$'}
                         buyNow={'BUY'}
                     />
                     <ProductItem
                         imgUrl={'https://source.unsplash.com/weekly?shoes'}
                         productTitle={'Product02'}
-                        productPrice={129}
+                        productPrice={129 + '$'}
                         buyNow={'BUY'}
                     />
                     <ProductItem
                         imgUrl={'https://source.unsplash.com/weekly?levis'}
                         productTitle={'Product03'}
-                        productPrice={89}
+                        productPrice={89 + '$'}
                         buyNow={'BUY'}
                     />
             </div>
